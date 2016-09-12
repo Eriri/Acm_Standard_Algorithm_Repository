9b584d3329f53d52bb16ae0585f031ece946b607
Eriri <741674999@qq.com>
2016-09-12 21:30:37 +0800

    Update 素数筛法.cpp

--- 素数筛法.cpp
+++ 素数筛法.cpp
@@ -1,25 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define maxn 
+#define maxn 1000
 
 bool ispri[maxn];
 int pri[maxn],pnum;
 
-void sieve(int n)//求n以内的素数 
+void sieve(int n)
 {
 	memset(ispri,0,sizeof(ispri));
 	memset(pri,0,sizeof(pri));
-	pnum=0;
-	ispri[0]=ispri[1]=true;
+	pnum=0;ispri[0]=ispri[1]=true;
 	for(int i=2;i<n;++i)
 	{
 		if(!ispri[i])pri[pnum++]=i;
 		for(int j=0;j<pnum&&i*pri[j]<n;++j)
-		{
-			ispri[i*pri[j]]=true;
-			if(i%pri[j]==0)break;
-		}
+			{ispri[i*pri[j]]=true;if(i%pri[j]==0)break;}
 	}
 }
 
