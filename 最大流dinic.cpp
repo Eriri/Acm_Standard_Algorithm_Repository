3f2db8444e5ca9b94ad4a34e504f2d9a132e2ceb
Eriri <741674999@qq.com>
2016-09-12 21:44:52 +0800

    Update 最大流dinic.cpp

--- 最大流dinic.cpp
+++ 最大流dinic.cpp
@@ -11,56 +11,41 @@ struct edge
     int vt,ne,cap;
 }E[maxm];
 
-int N[maxn],stk[maxm],level[maxn],n,s,t,top,maxflow,cnt;
+int N[maxn],stk[maxm],level[maxn],n,s,t,top,maxflow,cnt=2;
 
 bool build_level_network()
 {
-    for (int i=s;i<=t;++i) level[i]=inf;
-	level[s]=0;
-    queue<int> wfs;
-	int u,v,e;
-	wfs.push(s);
+    	for (int i=s;i<=t;++i) level[i]=inf;
+    	queue<int> wfs;
+	wfs.push(s);level[s]=0;
 	while (!wfs.empty())
 	{
-		u=wfs.front();
-		wfs.pop();
-		for(e=N[u],v=E[e].vt;e;e=E[e].ne,v=E[e].vt)
+		int u=wfs.front();wfs.pop();
+		for(int e=N[u];e;e=E[e].ne)
 		{
-			if(level[v]>level[u]+1&&E[e].cap)
-			{
-				level[v]=level[u]+1;
-				if(level[v]<level[t])wfs.push(v);
-			}
+			if(level[E[e].vt]>level[u]+1&&E[e].cap)
+				{level[E[e].vt]=level[u]+1;if(level[E[e].vt]<level[t])wfs.push(E[e].vt);}
 		}
 	}
 	return level[t]!=inf;
 }
 
-int update_flow(int n)
+int update_flow(int u)
 {
-    int e,v,f;
-	if(n==t)
+	int f;
+	if(u==t)
 	{
 		f=inf;
 		for(int i=0;i<top;++i)f=min(f,E[stk[i]].cap);
-		for(int i=0;i<top;++i)
-		{
-			E[stk[i]].cap-=f;
-            E[stk[i]^1].cap+=f;
-		}
+		for(int i=0;i<top;++i){E[stk[i]].cap-=f;E[stk[i]^1].cap+=f;}
 		return f;
 	}
 	else
 	{
 		f=0;
-		for(e=N[n];e;e=E[e].ne)
+		for(int e=N[u];e;e=E[e].ne)
 		{
-			if(level[E[e].vt]==level[n]+1)
-			{
-				stk[top++]=e;
-				f+=update_flow(E[e].vt);
-				--top;
-			}
+			if(level[E[e].vt]==level[u]+1){stk[top++]=e;f+=update_flow(E[e].vt);--top;}
 			else continue;
 		}
 
@@ -68,21 +53,6 @@ int update_flow(int n)
 	return f;
 }
 
-void dinic()
-{
-    while(build_level_network())
-    {
-		top=0;
-        maxflow+=update_flow(s);
-    }
-}
+void dinic(){while(build_level_network()){top=0;maxflow+=update_flow(s);}}
 
-int main()
-{
-    cnt=2;
-    build_map();//node_num [1...n]
-    s=0,t=n+1;
-    maxflow=0;
-    dinic();
-    printf("%d\n",maxflow);
-}
+int main(){}
