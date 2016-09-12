4bea989c8adf45970d7266ae6041604d71768896
Eriri <741674999@qq.com>
2016-09-12 21:49:53 +0800

    Update 强连通分量.cpp

--- 强连通分量.cpp
+++ 强连通分量.cpp
@@ -1,18 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define maxn 10000
-#define maxm 10000
-int top,ts,scc;
-int dfn[maxn],low[maxn];
-bool isse[maxn],instk[maxn];
+
+#define maxn 1000
+#define maxm 1000
+
+int top,ts,scc,dfn[maxn],low[maxn],N[maxn];
+bool vis[maxn],instk[maxn];
 stack<int> stk;
-int min(int a,int b){return a>b?b:a;}
-int max(int a,int b){return a>b?a:b;}
-struct node
-{
-	node():eid(0){};
-	int eid;
-}N[maxn];
+
 struct edge
 {
 	edge(){};
@@ -20,65 +15,24 @@ struct edge
 	int vt,ne;
 }E[maxm];
 
-void dfs(int n)
+void dfs(int u)
 {
-	stk.push(n);
-	isse[n]=instk[n]=true;
-	dfn[n]=low[n]=ts;
-	int i=N[n].eid;
-	while(i!=0)
+	stk.push(u);vis[u]=instk[u]=true;dfn[u]=low[u]=ts;
+	for(int e=N[u];e;e=E[e].ne)
 	{
-		if(isse[E[i].vt]==true)
-		{
-			if(instk[E[i].vt]==true)
-			{
-				low[n]=min(low[n],low[E[i].vt]);
-			}
-		}
-		else
-		{
-			++ts;
-			dfs(E[i].vt);
-			low[n]=min(low[n],low[E[i].vt]);
-		}
-		i=E[i].ne;
+		if(vis[E[e].vt]==true){if(instk[E[e].vt]==true)low[n]=min(low[n],low[E[e].vt]);}
+		else{++ts;dfs(E[e].vt);low[n]=min(low[n],low[E[e].vt]);}
 	}
 	if(low[n]==dfn[n])
 	{
 		++scc;
-		while(stk.top()!=n)
-		{
-			top=stk.top();
-			stk.pop();
-			instk[top]=false;
-		}
-		stk.pop();
-		instk[n]=false;
+		while(stk.top()!=n){top=stk.top();stk.pop();instk[top]=false;}
+		stk.pop();instk[n]=false;
 	}
 }
-void tarjan(int n)
+void tarjan(int u)
 {
 	while(!stk.empty())stk.pop();
-	ts=1;
-	dfs(n);
+	ts=1;dfs(u);
 }
-int main()
-{
-	memset(isse,0,sizeof(isse));
-	memset(N,0,sizeof(N));
-	memset(E,0,sizeof(E));
-	memset(instk,0,sizeof(instk));
-	memset(dfn,0,sizeof(dfn));
-	memset(low,0,sizeof(low));
-	
-	int n,m;
-	int u,v;
-	scanf("%d%d",&n,&m);
-	for(int i=0;i<m;++i)
-	{
-		scanf("%d%d",&u,&v);
-		E[cnt]=edge(v,N[u].eid);N[u].eid=cnt++;
-	}
-	for(int i=1;i<=n;++i) if(isse[i]==false)tarjan(i);
-	printf("%d\n",scc);
-}
+int main(){}
