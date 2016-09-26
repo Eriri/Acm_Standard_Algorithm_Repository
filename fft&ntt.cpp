a17d2084b8a5d829c5668c5a73fb1072f5aa0570
Eriri <741674999@qq.com>
2016-09-26 19:44:01 +0800

    n

--- fft&ntt.cpp
+++ fft&ntt.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 #define PI (acos(-1.0))
 typedef complex<double> cpd;
+typedef long long ll;
 
 void fft(cpd C[],int len,int s)
 {
@@ -24,6 +25,35 @@ void fft(cpd C[],int len,int s)
 			}
 		}
 	}
+}
+
+ll P=(17LL<<27)+1,G=3;//P=(15LL<<27)+1,G=31;
+ll fpow(ll b,ll p,ll m){ll r=1;for(;p;(p>>=1),b=b*b%m)if(p&1)r=r*b%m;return r;}
+
+void ntt(ll C[],int len,int s)
+{
+    int i,j,k,h;
+    for(i=j=0;i<len-1;++i,j|=k)
+    {
+        if(i<j)swap(C[i],C[j]);
+        for(k=len>>1;j>=k;j-=k,k>>=1);
+    }
+    ll wn,w,c,inv;
+    for(h=2;h<=len;h<<=1)
+    {
+        wn=fpow(G,(P-1)/h,P);
+        if(s==-1)wn=fpow(wn,P-2,P);
+        for(i=0;i<len;i+=h)
+        {
+            for(j=i,w=1;j<i+(h>>1);++j,w=wn*w%P)
+            {
+                c=w*C[j+(h>>1)]%P;
+                C[j+(h>>1)]=(C[j]-c+P)%P;
+            	C[j]=(C[j]+c)%P;
+			}
+        }
+    }
+    if(s==-1)for(i=0,inv=fpow(len,P-2,P);i<len;++i)C[i]=C[i]*inv%P;
 }
 
 void multiply(cpd A[],cpd B[],cpd C[],int len)
