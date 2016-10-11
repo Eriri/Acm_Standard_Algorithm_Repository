bd0fed5b2e78b3b5d41784edf6532e993640b2f6
Eriri <741674999@qq.com>
2016-10-11 23:52:36 +0800

    f

--- 最大流dinic.cpp
+++ 最大流dinic.cpp
@@ -1,58 +1,61 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define inf 0x7fffffff
-#define maxn 
-#define maxm 
+#define inf 0x3f3f3f3f
+#define maxn 10000
+#define maxm 10000
 
-struct edge
+struct dinic
 {
-    edge(){};
-    edge(int v,int n,int c):vt(v),ne(n),cap(c){};
-    int vt,ne,cap;
-}E[maxm];
-
-int N[maxn],stk[maxm],level[maxn],n,s,t,top,maxflow,cnt=2;
-
-bool build_level_network()
-{
-    	for (int i=s;i<=t;++i) level[i]=inf;
-    	queue<int> wfs;
-	wfs.push(s);level[s]=0;
-	while (!wfs.empty())
+	dinic(){};
+	struct edge
 	{
-		int u=wfs.front();wfs.pop();
-		for(int e=N[u];e;e=E[e].ne)
+		edge(){};
+		edge(int v,int n,int c):vt(v),ne(n),cap(c){};
+		int vt,ne,cap;
+	}E[maxm];
+	int N[maxn],stk[maxm],level[maxn],n,s,t,top,maxflow,cnt;
+	void init(){memset(N,0,sizeof(N));cnt=2;maxflow=0;}
+	void build(int u,int v,int c)
+	{E[cnt]=edge(v,N[u],c);N[u]=cnt++;E[cnt]=edge(u,N[v],0);N[v]=cnt++;}
+	bool build_level_network()
+	{
+		memset(level,inf,sizeof(level));
+		queue<int> wfs;
+		wfs.push(s);level[s]=0;
+		while (!wfs.empty())
 		{
-			if(level[E[e].vt]>level[u]+1&&E[e].cap)
-				{level[E[e].vt]=level[u]+1;if(level[E[e].vt]<level[t])wfs.push(E[e].vt);}
+			int u=wfs.front();wfs.pop();
+			for(int e=N[u];e;e=E[e].ne)
+			{
+				if(level[E[e].vt]>level[u]+1&&E[e].cap)
+					{level[E[e].vt]=level[u]+1;if(level[E[e].vt]<level[t])wfs.push(E[e].vt);}
+			}
+		}
+		return level[t]!=inf;
+	}
+	int update_flow(int u)
+	{
+		if(u==t)
+		{
+			int f=inf;
+			for(int i=0;i<top;++i)f=min(f,E[stk[i]].cap);
+			for(int i=0;i<top;++i){E[stk[i]].cap-=f;E[stk[i]^1].cap+=f;}
+			return f;
+		}
+		else
+		{
+			int f=0;
+			for(int e=N[u];e;e=E[e].ne)if(level[E[e].vt]==level[u]+1)
+			{stk[top++]=e;f+=update_flow(E[e].vt);--top;}
+			return f;
 		}
 	}
-	return level[t]!=inf;
-}
-
-int update_flow(int u)
-{
-	int f;
-	if(u==t)
+	int run()
 	{
-		f=inf;
-		for(int i=0;i<top;++i)f=min(f,E[stk[i]].cap);
-		for(int i=0;i<top;++i){E[stk[i]].cap-=f;E[stk[i]^1].cap+=f;}
-		return f;
+		while(build_level_network()){top=0;maxflow+=update_flow(s);}
+		return maxflow;
 	}
-	else
-	{
-		f=0;
-		for(int e=N[u];e;e=E[e].ne)
-		{
-			if(level[E[e].vt]==level[u]+1){stk[top++]=e;f+=update_flow(E[e].vt);--top;}
-			else continue;
-		}
+};
 
-	}
-	return f;
-}
-
-void dinic(){while(build_level_network()){top=0;maxflow+=update_flow(s);}}
 
 int main(){}
