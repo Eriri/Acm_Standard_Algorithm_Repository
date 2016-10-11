d738d59693e728d10d293f356f42d3363c795bf6
Eriri <741674999@qq.com>
2016-10-11 19:16:25 +0800

    a

--- AC自动机.cpp
+++ AC自动机.cpp
@@ -6,67 +6,43 @@ struct node
     node():suc(0),fail(0){memset(next,0,sizeof(next));}
     node *next[26],*fail;//efficient when the sentence only contain 
     int suc;			 //letters of alphabet
-}*root,*move,*facr;
+}*root=new node(),*move,*facr;
 
 deque<node*> q;
 
 void insert_string(string s)
 {
 	move=root;
-	for(int i=0;i<s.length();++i)
-	{
-		if(move->next[s[i]-'a']==NULL) move->next[s[i]-'a']=new node();
-		move=move->next[s[i]-'a'];
-	}
+	for(int i=0;i<s.length();move=move->next[s[i]-'a'],++i)
+		if(!move->next[s[i]-'a'])move->next[s[i]-'a']=new node();
 	++move->suc;
-}
-
-void inital_fail_pointer()
-{
-    for(int i=0;i<26;++i)
-    {
-        if(root->next[i]!=NULL)
-        {
-            root->next[i]->fail=root;
-            q.push_back(root->next[i]);
-        }
-    }
 }
 
 void build_fail_pointer()
 {
+	for(int i=0;i<26;++i)if(root->next[i])
+    	{root->next[i]->fail=root;q.push_back(root->next[i]);}
 	bool flag;
     while(!q.empty())
     {
-        move=q.front();
-        q.pop_front();
-        for(int i=0;i<26;++i)
+        move=q.front();q.pop_front();
+        for(int i=0;i<26;++i)if(move->next[i])
         {
-            if(move->next[i]!=NULL)
+            q.push_back(move->next[i]);facr=move->fail;flag=false;
+            while(facr)
             {
-                q.push_back(move->next[i]);
-                facr=move->fail;
-                flag=false;
-                while(facr!=NULL)
-                {
-                    if(facr->next[i]!=NULL)
-                    {
-                        move->next[i]->fail=facr->next[i];
-                        flag=true;
-                        break;
-                    }
-                    facr=facr->fail;
-                }
-                if(!flag) move->next[i]->fail=root;
+                if(facr->next[i])
+					{move->next[i]->fail=facr->next[i];flag=true;break;}
+                facr=facr->fail;
             }
+            if(!flag) move->next[i]->fail=root;
         }
     }
 }
 
 int matchingword(string p)
 {
-    int i,stas;
-	bool flag;
+    int i,stas;bool flag;
     for(i=0,move=root,stas=0;i!=p.length();++i)
     {
         flag=false;
@@ -74,38 +50,23 @@ int matchingword(string p)
         {
             if(move->next[p[i]-'a'])
             {
-                flag=true;
-                move=move->next[p[i]-'a'];
-                
-                //one time version
-                /*
+                flag=true;move=move->next[p[i]-'a'];
+                /*one time version
                 if(move->suc==-1) break;
-                stas+=move->suc;
-                move->suc=-1;
-				*/
-                
-                //more time version
-                /*
+                stas+=move->suc;move->suc=-1;
+                /*more time version
 				stas+=move->suc; 
-				*/
-				       
+				*/   
 				facr=move->fail;
                 while(facr)
                 {
-                     //one time version
-                    /*
+                    /*one time version
                     if(facr->suc==-1) break;
-                    stas+=facr->suc;
-                    facr->suc=-1;
-                    */
-                    
-                    //more time version
-                    /*
+                    stas+=facr->suc;facr->suc=-1;
+                    /*more time version
                     stas+=facr->suc;
                     */
-                    
                     facr=facr->fail;
-                    
                 }
                 break;
             }
@@ -116,23 +77,4 @@ int matchingword(string p)
     return stas;
 }
 
-int main()
-{
-    int n,stas;
-    string p;
-    
-    root=new node();
-    scanf("%d",&n);
-    for(int i=0;i<n;++i)
-    {
-        cin>>p;
-        insert_string(p);
-    }
-    inital_fail_pointer();
-    build_fail_pointer();
-    
-    cin>>p;
-    stas=matchingword(p);
-    
-    printf("%d\n",stas);
-}
+int main(){}
