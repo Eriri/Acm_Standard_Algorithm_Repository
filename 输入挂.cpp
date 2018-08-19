89f2ae2d65e6147b5bd997a8b570cb558e18ddf7
Eriri <741674999@qq.com>
2018-08-19 21:29:03 +0800

    1

--- 输入挂.cpp
+++ 输入挂.cpp
@@ -4,8 +4,7 @@ const int bsize = 1<<15 ;
 char buf[bsize],*ps=buf,*pe=buf+1;
 #define buf_in if(++ps==pe)pe=(ps=buf)+fread(buf,1,sizeof(buf),stdin)
 template<class T>
-inline bool CIN(T &ans)
-{
+inline bool CIN(T &ans){
     ans=0;T op=1;
     if(ps==pe)return false;
     do{
@@ -18,7 +17,6 @@ inline bool CIN(T &ans)
         buf_in;
     }while(isdigit(*ps)&&ps!=pe);
     ans*=op;
-    return true;
-}
+    return true;}
 
 int main(){}
